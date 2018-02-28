717bd2859660f1d966127548bd7ca2e6075be356
Christopher Lee <revisedboomerguy@gmail.com>
2018-02-28 14:12:32 -0500

    Change all code to Nest MVC example

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+
+@Module({
+    components: [],
+    controllers: [AppController],
+    imports: []
+})
+export class ApplicationModule {}
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from "@nestjs/core";
+import * as express from "express";
+import * as path from "path";
+import { ApplicationModule } from "./app.module";
+
+async function bootstrap() {
+    const app = await NestFactory.create(ApplicationModule);
+
+    app.use(express.static(path.join(__dirname, "public")));
+    app.set("views", __dirname + "/views");
+    app.set("view engine", "hbs");
+
+    await app.listen(8080);
+}
+bootstrap();

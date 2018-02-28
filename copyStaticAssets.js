3a5812bec4d2652edc58c8ac04263d82ad5b7404
Christopher Lee <revisedboomerguy@gmail.com>
2018-02-28 13:35:17 -0500

    Update building process to use Nest recommended

--- copyStaticAssets.js
+++
@@ -1,7 +0,0 @@
-const shell = require("shelljs");
-
-// shell.cp("-R", "src/public/js/lib", "dist/public/js/");
-// shell.cp("-R", "src/public/fonts", "dist/public/");
-// shell.cp("-R", "src/public/images", "dist/public/");
-shell.mkdir("dist/public/");
-shell.cp("-R", "src/public/stylesheets", "dist/public/stylesheets/");

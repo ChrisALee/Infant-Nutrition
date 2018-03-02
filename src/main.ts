c0fc0934575b08a62c3deedd26d95968b48cded1
Christopher Lee <revisedboomerguy@gmail.com>
2018-03-01 19:14:25 -0500

    Use new lint rules

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from "@nestjs/core";
-import * as express from "express";
-import * as path from "path";
-import { ApplicationModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import * as path from 'path';
+import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
 
-    app.use(express.static(path.join(__dirname, "public")));
-    app.set("views", __dirname + "/views");
-    app.set("view engine", "hbs");
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'hbs');
 
     await app.listen(8080);
 }

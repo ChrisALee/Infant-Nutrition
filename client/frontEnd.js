4ed8fe8035c4ee0c756d21dd21394d6d8022219c
Scott Krawczyk <skrawczyk1000@gmail.com>
2018-03-06 20:32:43 -0500

    Front end upload

---
+++ client/frontEnd.js
@@ -0,0 +1,19 @@
+function main() {
+    window.onclick = function (event) {
+        var elmt = event.target;
+        var num = parseInt(elmt.id);
+        if (0 <= num && num <= 6) {
+            changeInfo(num);
+        }
+    };
+    function changeInfo(num) {
+        var selectedStage = document.getElementById("" + num + "");
+        var info = document.getElementById("" + num + "0");
+        var stageName = document.getElementById("stageName");
+        var stageInfo = document.getElementById("stageInfo");
+        stageInfo.style.visibility = "visible";
+        stageName.innerHTML = selectedStage.innerHTML;
+        stageInfo.innerHTML = info.innerHTML;
+    }
+}
+main();

e0ade584adb1f5441957dc80aaba9c0d1ee8c999
Christopher Lee <revisedboomerguy@gmail.com>
2018-03-27 00:07:06 -0400

    Tear down user data in Redis after tests

--- api/src/utils/test-environment.ts
+++ api/src/utils/test-environment.ts
@@ -1,6 +1,9 @@
 import { deployment } from '../app.js';
+import * as redisTear from 'redis';
 
 const NodeEnvironment = require('jest-environment-node');
+
+require('dotenv').config();
 
 class TestEnvironment extends NodeEnvironment {
     async setup() {
@@ -9,6 +12,8 @@ class TestEnvironment extends NodeEnvironment {
 
         try {
             await super.setup();
+
+            // Setup server and make it global in the test environment
             this.global.server = await deployment(false);
         } catch (err) {
             throw err;
@@ -16,7 +21,21 @@ class TestEnvironment extends NodeEnvironment {
     }
 
     async teardown() {
-        await super.teardown();
+        try {
+            const redisClientTear = await redisTear.createClient(
+                `redis://:${process.env.REDIS_PASS}@${process.env.REDIS_HOST}:${
+                    process.env.REDIS_PORT
+                }/${process.env.REDIS_DB}`,
+            );
+            // Tear down Redis user data
+            await redisClientTear.FLUSHDB();
+
+            redisClientTear.quit();
+
+            await super.teardown();
+        } catch (err) {
+            throw err;
+        }
     }
 
     runScript(script) {

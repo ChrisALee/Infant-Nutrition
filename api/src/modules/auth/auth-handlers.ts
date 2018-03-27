036ebac9eece04736b9330a4912332be90d82ec2
Christopher Lee <revisedboomerguy@gmail.com>
2018-03-27 05:39:29 -0400

    Implement auth tests and fix code smells found from tests

--- api/src/modules/auth/auth-handlers.ts
+++ api/src/modules/auth/auth-handlers.ts
@@ -6,142 +6,160 @@ import generate = require('nanoid/generate');
 import url = require('nanoid/url');
 
 import Knex from '../../utils/knex';
+import Redis from '../../utils/redis';
 
 export const login = async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
-    try {
-        const { user }: any = request.payload;
+    const { user }: any = request.payload;
+    let results;
 
-        const results = await Knex('users')
+    try {
+        // Use supplied credentials to fetch user from database
+        results = await Knex('users')
             .where({
                 username: user.username,
             })
             .first();
 
+        // Invalid user was supplied and was unable to fetch from database
         if (!results || results.length === 0) {
-            return {
-                error: true,
-                errMessage: 'no user found',
-            };
+            throw 'Invalid username';
         }
 
         const isValid = await Bcrypt.compare(user.password, results.password);
 
         if (!isValid) {
-            return 'wrong password';
+            throw 'Invalid password';
         }
-
-        const guid = generate(url, 10);
-
-        const session = {
-            valid: true,
-            userGuid: results.guid,
-            username: results.username,
-            email: results.email,
-            guid,
-        };
-        // create the session in Redis
-        const redisClient = (request as any).redis.client;
-        try {
-            await redisClient.set(session.guid, JSON.stringify(session));
-        } catch (err) {
-            request.log('auth', err);
-            throw Boom.internal('Internal Redis error');
-        }
-
-        // TODO: set a good expiresIn for access token after refresh token
-        // is implemented
-        const token = JWT.sign({ guid: session.guid }, process.env.JWT_KEY, {
-            expiresIn: '30h',
-        });
-
-        return h
-            .response({ text: 'Check Auth Header for your Token' })
-            .type('application/json')
-            .header('Authorization', token)
-            .state('token', token);
     } catch (err) {
         request.log('auth', err);
-        throw Boom.internal('Internal database error');
+        throw Boom.unauthorized('Invalid credentials');
     }
+
+    // Redis session data
+    const guid = generate(url, 10);
+    const session = {
+        valid: true,
+        userGuid: results.guid,
+        username: results.username,
+        email: results.email,
+        guid,
+    };
+
+    // Set the session in Redis
+    try {
+        await Redis.set(session.guid, JSON.stringify(session));
+    } catch (err) {
+        request.log('auth', err);
+        throw Boom.internal('Internal Redis error');
+    }
+
+    // TODO: set a good expiresIn for access token after refresh token
+    // is implemented
+    const token = JWT.sign({ guid: session.guid }, process.env.JWT_KEY, {
+        expiresIn: '30h',
+    });
+
+    return h
+        .response({ text: 'Check Auth Header for your Token' })
+        .type('application/json')
+        .header('Authorization', token)
+        .state('token', token);
 };
 
 export const logout = async (
     request: Hapi.Request,
     h: Hapi.ResponseToolkit,
 ) => {
+    let redisResult;
     try {
-        const redisClient = (request as any).redis.client;
-
-        const redisResult = await redisClient.get(
+        // Get Redis session
+        redisResult = await Redis.get(
             (request as any).auth.credentials.sessionGuid,
         );
-
-        const session = JSON.parse(redisResult);
-        // update the session to no longer valid:
-        session.valid = false;
-        session.ended = new Date().getTime();
-        // create the session in Redis
-        await redisClient.set(session.guid, JSON.stringify(session));
-
-        return h
-            .response({ text: 'You have been logged out' })
-            .unstate('token');
     } catch (err) {
         request.log('auth', err);
-        throw Boom.internal('Internal database error');
+        throw Boom.badRequest(err);
     }
+
+    if (!redisResult || redisResult.length === 0) {
+        throw Boom.unauthorized('Invalid credentials');
+    }
+
+    const session = JSON.parse(redisResult);
+
+    // update the session to no longer valid
+    session.valid = false;
+    session.ended = new Date().getTime();
+
+    try {
+        // create the session in Redis
+        await Redis.set(session.guid, JSON.stringify(session));
+    } catch (err) {
+        request.log('auth', err);
+        throw Boom.internal('Internal Redis error');
+    }
+
+    return h.response({ text: 'You have been logged out' }).unstate('token');
 };
 
 export const register = async (
     request: Hapi.Request,
     h: Hapi.ResponseToolkit,
 ) => {
+    const { user }: any = request.payload;
+    const userGuid = generate(url, 10);
+    let saltedPass;
+
+    // Salt the password
     try {
-        const { user }: any = request.payload;
-        const userGuid = generate(url, 10);
+        saltedPass = await Bcrypt.hash(user.password, 10);
+    } catch (err) {
+        request.log('auth', err);
+        throw Boom.internal('Failed to salt password');
+    }
 
-        const saltedPass = await Bcrypt.hash(user.password, 10);
+    // Create a new user object with all final data for database
+    const userWithGuidAndSalt = {
+        ...user,
+        password: saltedPass,
+        guid: userGuid,
+    };
 
-        const userWithGuidAndSalt = {
-            ...user,
-            password: saltedPass,
-            guid: userGuid,
-        };
-
+    try {
         await Knex('users').insert(userWithGuidAndSalt);
-
-        const guid = generate(url, 10);
-
-        const session = {
-            valid: true,
-            userGuid: userWithGuidAndSalt.guid,
-            username: userWithGuidAndSalt.username,
-            name: userWithGuidAndSalt.username,
-            email: userWithGuidAndSalt.email,
-            guid,
-        };
-        // create the session in Redis
-        const redisClient = (request as any).redis.client;
-        try {
-            await redisClient.set(session.guid, JSON.stringify(session));
-        } catch (err) {
-            request.log('auth', err);
-            throw Boom.internal('Internal Redis error');
-        }
-
-        // TODO: set a good expiresIn for access token after refresh token
-        // is implemented
-        const token = JWT.sign({ guid: session.guid }, process.env.JWT_KEY, {
-            expiresIn: '30h',
-        });
-
-        return h
-            .response({ text: 'Check Auth Header for your Token' })
-            .type('application/json')
-            .header('Authorization', token)
-            .state('token', token);
     } catch (err) {
         request.log('auth', err);
         throw Boom.internal('Internal database error');
     }
+
+    const guid = generate(url, 10);
+
+    const session = {
+        valid: true,
+        userGuid: userWithGuidAndSalt.guid,
+        username: userWithGuidAndSalt.username,
+        name: userWithGuidAndSalt.username,
+        email: userWithGuidAndSalt.email,
+        guid,
+    };
+
+    // Set the session in Redis
+    try {
+        await Redis.set(session.guid, JSON.stringify(session));
+    } catch (err) {
+        request.log('auth', err);
+        throw Boom.internal('Internal Redis error');
+    }
+
+    // TODO: set a good expiresIn for access token after refresh token
+    // is implemented
+    const token = JWT.sign({ guid: session.guid }, process.env.JWT_KEY, {
+        expiresIn: '30h',
+    });
+
+    return h
+        .response({ text: 'Check Auth Header for your Token' })
+        .type('application/json')
+        .header('Authorization', token)
+        .state('token', token);
 };
--- api/src/modules/auth/auth-test.ts
+++ api/src/modules/auth/auth-test.ts
@@ -1,15 +1,179 @@
-describe('auth tests', async () => {
-    describe('GET /auth/login', () => {
-        test('should return 200 OK', async () => {
-            const injectOptions = {
-                method: 'GET',
-                url: '/api/v1/quizzes',
-            };
-            let server = global['server'];
+describe('Auth', () => {
+    const server = global['server'];
+    const Redis = global['Redis'];
 
-            const response = await server.inject(injectOptions);
+    describe('Login', () => {
+        describe('POST /auth/login', () => {
+            test('should login in as user when correct payload', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/login',
+                    payload: {
+                        user: {
+                            username: 'clee',
+                            password: 'password1',
+                        },
+                    },
+                };
 
-            expect(response.statusCode).toBe(200);
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(200);
+            });
+
+            test('should not login as user when incorrect payload', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/login',
+                    payload: {
+                        user: {
+                            username: 'cle231je',
+                            password: 'passwo312312rd1',
+                        },
+                    },
+                };
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(401);
+            });
+
+            test('should not login as user when malformed payload', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/login',
+                    payload: {
+                        user: {
+                            username: 'clee',
+                        },
+                    },
+                };
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(400);
+            });
+        });
+    });
+
+    describe('Logout', () => {
+        describe('POST /auth/logout', () => {
+            beforeEach(async () => {
+                await Redis.flushdb();
+            });
+
+            test('should logout when correct session credentials', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/logout',
+                    // Bypass validation by sending credentials
+                    credentials: {
+                        sessionGuid: 0,
+                    },
+                };
+
+                await Redis.set(0, JSON.stringify({ guid: 0, valid: true }));
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(200);
+            });
+
+            test('should not logout when incorrect session credentials', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/logout',
+                    // Bypass validation by sending credentials
+                    credentials: {
+                        sessionGuid: 5,
+                    },
+                };
+
+                await Redis.set(1, JSON.stringify({ guid: 1, valid: true }));
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(401);
+            });
+
+            test('should not logout when malformed session credentials', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/logout',
+                    // Bypass validation by sending credentials
+                    credentials: {
+                        malformedItem: 5,
+                    },
+                };
+
+                await Redis.set(2, JSON.stringify({ guid: 2, valid: true }));
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(400);
+            });
+        });
+    });
+
+    describe('Register', () => {
+        describe('POST /auth/register', () => {
+            test('should register new user when correct payload', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/register',
+                    payload: {
+                        user: {
+                            username: '1234',
+                            password: '1234',
+                            name: '1234',
+                            email: '1234',
+                            // TODO: Uncomment when JOI validators are hardened
+                            // email: '1234@email.com'
+                        },
+                    },
+                };
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(200);
+            });
+
+            // TODO: Uncomment when JOI validators are hardened
+            // test('should not register new user when incorrect payload', async () => {
+            //     const injectOptions = {
+            //         method: 'POST',
+            //         url: '/auth/register',
+            //         payload: {
+            //             user: {
+            //                 username: '12345',
+            //                 password: '12345',
+            //                 name: '12345',
+            //                 email: '12345',
+            //             },
+            //         },
+            //     };
+
+            //     const response = await server.inject(injectOptions);
+
+            //     expect(response.statusCode).toBe(200);
+            // });
+
+            test('should not register new user when malformed payload', async () => {
+                const injectOptions = {
+                    method: 'POST',
+                    url: '/auth/register',
+                    payload: {
+                        user: {
+                            username: '1234567',
+                            malformedItem: '1234567',
+                        },
+                    },
+                };
+
+                const response = await server.inject(injectOptions);
+
+                expect(response.statusCode).toBe(400);
+            });
         });
     });
 });

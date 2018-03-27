1f812cb2ba2ea5e538aacb386496f372679db382
Christopher Lee <revisedboomerguy@gmail.com>
2018-03-27 19:54:02 -0400

    Rename env vars for Redis to avoid conflicts

--- api/src/utils/redis.ts
+++ api/src/utils/redis.ts
@@ -4,10 +4,10 @@ let Redis;
 
 if (process.env.NODE_ENV !== 'test') {
     Redis = require('ioredis')({
-        port: process.env.REDIS_PORT || 6379,
-        host: process.env.REDIS_HOST,
-        password: process.env.REDIS_PASS,
-        db: process.env.REDIS_DB,
+        port: process.env.REDIS_DB_PORT || 6379,
+        host: process.env.REDIS_DB_HOST,
+        password: process.env.REDIS_DB_PASS,
+        db: process.env.REDIS_DB_NAME,
     });
 } else {
     // Pulls from Test Environment setup file

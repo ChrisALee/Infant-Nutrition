60283ef064c4227ba2a077fe3cfff5aa304f5872
Christopher Lee <revisedboomerguy@gmail.com>
2018-03-27 20:35:25 -0400

    Remove debug logs

--- api/src/modules/user/user-handlers.ts
+++ api/src/modules/user/user-handlers.ts
@@ -49,25 +49,19 @@ export const postUser = async (
     };
 
     // Set the session in Redis
-    console.log('Made it to before Redis');
     try {
-        console.log(Redis);
         await Redis.set(session.guid, JSON.stringify(session));
-        console.log('Attempted Redis');
     } catch (err) {
-        console.log('Made it to error: ', err);
         request.log('auth', err);
         throw Boom.internal('Internal Redis error');
     }
 
-    console.log('Right before JWT sign');
     // TODO: set a good expiresIn for access token after refresh token
     // is implemented
     const token = JWT.sign({ guid: session.guid }, process.env.JWT_KEY, {
         expiresIn: '30h',
     });
 
-    console.log('Right before returning', token);
     return h
         .response({ text: 'Check Auth Header for your Token' })
         .type('application/json')

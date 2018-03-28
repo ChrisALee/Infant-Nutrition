0b52c021de16e116ef7f4752e220884033d3e574
Carrie Storch <carriems@live.unc.edu>
2018-03-28 10:12:05 -0400

    Update newborn.tsx

--- client/components/newborn.tsx
+++ client/components/newborn.tsx
@@ -1,1 +1,9 @@
-export default props => <div id="00" />;
+export default props => (
+	<div id="00">
+		<h3> Developmental Milestones: </h3>
+		<ul>
+                <li>Insert newborn data here.</li>
+            </ul>
+        </div>
+
+);

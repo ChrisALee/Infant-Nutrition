8cd9ca4fb8ecdf59b42a08e8f9973dfbc8cb8e79
Christopher Lee <revisedboomerguy@gmail.com>
2018-04-15 22:03:00 -0400

    Implement webpack bundle analyzer

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { ANALYZE } = process.env;
 
 const prod = process.env.NODE_ENV === 'production';
 
@@ -10,6 +12,16 @@ module.exports = withTypescript({
         // Do not run type checking twice:
         if (options.isServer)
             config.plugins.push(new ForkTsCheckerWebpackPlugin());
+
+        if (ANALYZE) {
+            config.plugins.push(
+                new BundleAnalyzerPlugin({
+                    analyzerMode: 'server',
+                    analyzerPort: options.isServer ? 8888 : 8889,
+                    openAnalyzer: true,
+                }),
+            );
+        }
 
         return config;
     },

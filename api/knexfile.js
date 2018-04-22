385027b87df3d0b6ef8e1c38dc3d360272aa2990
Christopher Lee <revisedboomerguy@gmail.com>
2018-04-22 18:21:27 -0400

    Try more changes to see if it fixes prod

--- api/knexfile.js
+++ api/knexfile.js
@@ -6,14 +6,17 @@ module.exports = {
         seeds: { tableName: './seeds' },
 
         client: process.env.DB_TYPE,
-        connection: {
-            host: process.env.DB_HOST,
+        connection:
+            process.env.NODE_ENV !== 'production'
+                ? {
+                      host: process.env.DB_HOST,
 
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
+                      user: process.env.DB_USER,
+                      password: process.env.DB_PASS,
 
-            database: process.env.DB,
-            charset: 'utf8',
-        },
+                      database: process.env.DB,
+                      charset: 'utf8',
+                  }
+                : process.env.PROD_DB_HOST,
     },
 };
--- api/src/utils/knex.ts
+++ api/src/utils/knex.ts
@@ -4,15 +4,18 @@ if (process.env.NODE_ENV !== 'test') {
     /* tslint:disable:no-var-requires */
     Knex = require('knex')({
         client: process.env.DB_TYPE,
-        connection: {
-            host: process.env.DB_HOST,
+        connection:
+            process.env.NODE_ENV !== 'production'
+                ? {
+                      host: process.env.DB_HOST,
 
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
+                      user: process.env.DB_USER,
+                      password: process.env.DB_PASS,
 
-            database: process.env.DB,
-            charset: 'utf8',
-        },
+                      database: process.env.DB,
+                      charset: 'utf8',
+                  }
+                : process.env.PROD_DB_HOST,
     });
 } else {
     // Pulls from Test Environment setup file
